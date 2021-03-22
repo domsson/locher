d6d6c646cc67e4c0daf69078a5d8ec5a1abadbd7
domsson <julien.d.dau@gmail.com>
2021-03-23 04:03:48 +0900

    Quite useful now, I think

--- src/locher.py
+++ src/locher.py
@@ -13,11 +13,33 @@ from version import *
 # notes
 #
 
-# w = hole width     (lochweite, lochgroesse vertikal / y)
-# l = hole length    (lochlaenge, lochgroesse horizontal / x)
-# p = hole partition (lochteilung, abstand von lochmitte zu lochmitte)
-# c = hole clearance (lochabstand, abstand von lochrand zu lochrand)
+# Hole types according to DIN 24041:
 #
+# Rv  = Rundlochung in versetzten Reihen
+# Rd  = Rundlochung in diagonal versetzten Reihen
+# Rg  = Rundlochung in geraden Reihen
+# Qg  = Quadratlochung in versetzten Reihen
+# Qd  = Quadratlochung in diagonal versetzten Reihen
+# Qg  = Quadratlochung in geraden Reihen
+# Lv  = Langlochung in versetzten Reihen
+# Lg  = Langlochung in geraden Reihen
+# Lge = Langlochung in geraden Reihen, eckig
+
+# Hole pattern parameters according to DIN 24041:
+#
+# w  = hole width, vertical         (lochweite, lochgroesse vertikal / y)
+# l  = hole length, horizontal      (lochlaenge, lochgroesse horizontal / x)
+# p  = hole partition               (lochteilung, abstand von lochmitte zu lochmitte)
+# c  = hole clearance               (lochabstand, abstand von lochrand zu lochrand)
+# e1 = hole free area at the bottom (ungelochter rand)
+# e2 = hole free area at the top    (ungelochter rand)
+# f1 = hole free area on the left   (ungelochter rand)
+# f2 = hole free area on the right  (ungelochter rand)
+# a1 = plate size, vertical / y     (plattenmaß, plattenlaenge)
+# b1 = plate size, horizontal / x   (plattenmaß, plattenbreite)
+# s  = plate strength               (plattendicke)
+# A0 = relative hole free area      (relative freie Lochflaeche)
+
 # uniform:   make rows have equal length (only applies to staggered patterns)
 # staggered: holes of every other row are offset (Rv, Qv, etc)
 # corners:   start with a hole in the left corner (only applies to staggered patterns)
@@ -41,6 +63,8 @@ default_fill            = "none"
 default_stroke          = "#000"
 default_stroke_width    = 1
 default_stroke_linejoin = "round"
+default_font_family     = "sans-serif"
+default_font_size       = "3"
 
 default_uniform = False
 default_corners = False
@@ -74,6 +98,11 @@ def draw_hole_l(svg, cfg, x, y, w, h):
     top  = y - h * 0.5
     radius = h * 0.5
     svg.add(svg.rect((left, top), (w, h), radius, radius, **cfg))
+
+def draw_hole_le(svg, cfg, x, y, w, h):
+    left = x - w * 0.5
+    top  = y - h * 0.5
+    svg.add(svg.rect((left, top), (w, h), **cfg))
 
 def draw_pattern(svg, sw, sh, wx, wy, px, py, hole_func, cfg, **opt):
     uniform = opt["uniform"]
@@ -109,6 +138,13 @@ def draw_pattern(svg, sw, sh, wx, wy, px, py, hole_func, cfg, **opt):
         for col in range((mod * skip), (hx - less)):
             x = sx + (col * px) + (mod * px * 0.5)
             hole_func(svg, cfg, x, y, hwx, hwy)
+
+def draw_ruler(svg, sw, sh, cfg):
+    path_extras = {"stroke": cfg["stroke"], "stroke-width": 1, "fill":"none"}
+    text_extras = {"font-family": "sans-serif", "font-size": 3, "fill": cfg["stroke"]}
+    path = "M" + str(sw-12) + "," + str(sh-3) + " v2 h10 v-2"
+    svg.add(svg.path(path, **path_extras))
+    svg.add(svg.text('10 mm', insert=(sw-25, sh-1), **text_extras))
 
 #
 # import external dependencies we can't expect to be installed
@@ -147,14 +183,13 @@ print(" Version " + Version.get_version() + " (using svgwrite " + svgwrite.__ver
 #
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--hole-type",        required = True)
-parser.add_argument("-w", "--hole-width",       required = True,  type = int) # hole size y
-parser.add_argument("-l", "--hole-length",      required = False, type = int) # hole size x
-parser.add_argument("-p", "--hole-partition-y", required = True,  type = int) # hole partition y
-parser.add_argument("-q", "--hole-partition-x", required = False, type = int) # hole partition x
-parser.add_argument("-u", "--uniform",          default = default_uniform)
-parser.add_argument("-c", "--corners",          default = default_corners)
-parser.add_argument("-s", "--skip",             default = default_skip, type = int)
+parser.add_argument("-t", "--hole-type",      required = True)
+parser.add_argument("-w", "--hole-width",     required = True,  type = int) # hole size y
+parser.add_argument("-l", "--hole-length",    required = False, type = int) # hole size x
+parser.add_argument("-p", "--hole-partition", required = True,  type = int) # hole partition
+parser.add_argument("-u", "--uniform",        default = default_uniform)
+parser.add_argument("-c", "--corners",        default = default_corners)
+parser.add_argument("-s", "--skip",           default = default_skip, type = int)
 parser.add_argument("-x", "--image-width",  default = default_image_width,  type = int)
 parser.add_argument("-y", "--image-height", default = default_image_height, type = int)
 parser.add_argument("-f", "--filename",     default = default_filename)
@@ -162,6 +197,7 @@ parser.add_argument("--fill",            default = default_fill,   metavar = "FI
 parser.add_argument("--stroke",          default = default_stroke, metavar = "STROKE-COLOR")
 parser.add_argument("--stroke-width",    default = default_stroke_width,    type = int)
 parser.add_argument("--stroke-linejoin", default = default_stroke_linejoin)
+parser.add_argument("--font-size",       default = default_font_size)
 
 args = parser.parse_args()
 
@@ -183,8 +219,8 @@ config = {
 pattern = {
         "w": args.hole_width,
         "l": args.hole_length if args.hole_length else args.hole_width,
-        "p": args.hole_partition_y,
-        "q": args.hole_partition_x if args.hole_partition_x else args.hole_partition_y
+        "p": args.hole_partition,
+        "q": args.hole_partition
 }
 
 options = {
@@ -197,7 +233,11 @@ options = {
 hole_func = None
 hole_type = args.hole_type.lower()
 
-if   (hole_type == "rd"):
+if   (hole_type == "rg"):
+    hole_func = draw_hole_r
+    pattern["q"] = pattern["p"]
+    options["staggered"] = False
+elif (hole_type == "rd"):
     hole_func = draw_hole_r
     pattern["q"] = math.sqrt(2) * pattern["q"]
     pattern["p"] = pattern["q"] * 0.5
@@ -206,47 +246,33 @@ elif (hole_type == "rv"):
     hole_func = draw_hole_r
     pattern["p"] = math.sqrt(3) * 0.5 * pattern["q"]
     options["staggered"] = True
-elif (hole_type == "rg"):
-    hole_func = draw_hole_r
+elif (hole_type == "qg"):
+    hole_func = draw_hole_q
+    pattern["q"] = pattern["p"]
     options["staggered"] = False
 elif (hole_type == "qd"):
     hole_func = draw_hole_q
-    # TODO calc p/q
-    # TODO rotate holes?
+    pattern["q"] = math.sqrt(2) * pattern["p"]
+    pattern["p"] = pattern["q"] * 0.5
     options["staggered"] = True
-    abort("Hole type " + hole_type + " not implemented", anykey=cfg_anykey_before_abort)
 elif (hole_type == "qv"):
     hole_func = draw_hole_q
-    # TODO calc p/q
+    pattern["q"] = pattern["p"]
     options["staggered"] = True
-    abort("Hole type " + hole_type + " not implemented", anykey=cfg_anykey_before_abort)
-elif (hole_type == "qg"):
-    hole_func = draw_hole_q
-    options["staggered"] = False
-elif (hole_type == "hv"):
-    abort("Hole type " + hole_type + " not implemented", anykey=cfg_anykey_before_abort)
-elif (hole_type == "lg"  or hole_type == "lgl"):
+elif (hole_type == "lg"):
     hole_func = draw_hole_l
     options["staggered"] = False
-elif (hole_type == "lv"  or hole_type == "lvl"):
+elif (hole_type == "lge"):
+    hole_func = draw_hole_le
+    options["staggered"] = False
+elif (hole_type == "lv"):
     hole_func = draw_hole_l
     options["staggered"] = True
-elif (hole_type == "lge" or hole_type == "lgel"):
-    hole_func = draw_hole_l
-    options["staggered"] = False
-elif (hole_type == "lve" or hole_type == "lvel"):
-    hole_func = draw_hole_l
+elif (hole_type == "lve"):
+    hole_func = draw_hole_le
     options["staggered"] = True
 else:
-    abort("Hole type " + hole_type + " not implemented", anykey=cfg_anykey_before_abort)
-#elif (hole_type is "lvq"):
-    #
-#elif (hole_type is "lgq"):
-    #
-#elif (hole_type is "lgeq"):
-    #
-#elif (hole_type is "lveq"):
-    #
+    abort("Hole type " + args.hole_type + " not implemented", anykey=cfg_anykey_before_abort)
 
 #
 # Core
@@ -254,11 +280,12 @@ else:
 
 svg = svgwrite.Drawing(args.filename, size=(svg_width, svg_height), viewBox=svg_viewbox, profile='tiny')
 draw_pattern(svg, args.image_width, args.image_height, pattern["l"], pattern["w"], pattern["q"], pattern["p"], hole_func, config, **options)
+draw_ruler(svg, args.image_width, args.image_height, config)
 svg.save(pretty=True, indent=4) 
 
 #
 # Done
 #
 
-abort("Done! SVG written to " + args.filename", anykey=cfg_anykey_before_abort)
+abort("Done! SVG written to " + args.filename, anykey=cfg_anykey_before_abort)
 
